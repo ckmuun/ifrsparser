03e1d4097fcc6344157aa574789b48e35dc72766
ckmuun <cornelius.koller@online.de>
2021-05-21 11:06:31 +0200

    modify scripts

--- acc-to-ex.sh
+++ acc-to-ex.sh
@@ -1,46 +1,55 @@
-
 #!/bin/bash
 echo "transforming files"
 
-mkdir output
-cd output
-for file in *
-do
-	rm $file
-done
-cd ..
-cd annual-reports-2020
-rm processed.txt
 
-touch processed.a
+# "evolution stages" of processing. Kinda clumsy but need for ref
+rm processedA.txt
+rm processedB.txt
+rm processedC.txt
+rm processedD.txt
 
-for file in *txt
-do
-	echo "processing $file"
-	COMPANY=$(echo $file)
-
-	COMPANY=$(echo $file | sed 's/\-layout-crop.txt//')
+touch processedA.txt
+touch processedB.txt
+touch processedC.txt
+touch processedD.txt
 
 
-cat $COMPANY >> processed.a
-
-echo "company is $COMPANY"
-
-echo "upper is $upper"
+#sed -E 's/ä/a/g' result.txt | cat >> processedA.txt
 
 
-	cat $file | sed  's/\   */|/g'  >> processed.a
-	# 's/[[:blank:]]*$//'; '/^$/d' $file-processed.txt
-	echo "done sed processing of file $file"
-	echo ""
-	echo ""
+sed -E 's/[ä]/ae/g ; s/[ü]/ue/g; s/[ö]/oe/g ; s/Ü/UE/g ; s/Ä/AE/g; s/Ö/OE/g; s/ß/ss/g'   result.txt >> processedA.txt
 
-	echo "##########" >> processed.a
-done
+
+#cat result.txt | sed 's/\  */d'
+
+
+#
+#for file in *txt
+#do
+#	echo "processing $file"
+#	COMPANY=(echo "$file")
+#
+#	COMPANY=$(echo $file | sed 's/\-layout-crop.txt//')
+#
+#
+#cat $COMPANY >> processed.a
+#
+#echo "company is $COMPANY"
+#
+#echo "upper is $upper"
+#
+#
+#	cat $file | sed  's/\   */|/g'  >> processed.a
+#	# 's/[[:blank:]]*$//'; '/^$/d' $file-processed.txt
+#	echo "done sed processing of file $file"
+#	echo ""
+#	echo ""
+#
+#	echo "##########" >> processed.a
+#done
 
 
 # cleanup processing results
 
 # sed 's/\![0-9a-z]//d' processed.a processed.b
 
-mv processed.a processed.txt
--- pdf-to-txt.sh
+++ pdf-to-txt.sh
@@ -1,10 +1,24 @@
 echo "converting pdfs to txt"
 
+for filetxt in *txt
+do
+  rm $filetxt
+done
+
+
+touch result.txt
+
+# iterate over pdfs in directory
 for file in *pdf
 
 do
 	echo "converting $file"
 	pdftotext -layout $file $file.txt
 
+  echo  "$file" >> result.txt
+  cat "$file".txt >> result.txt
+
+
 done
 
+cat result.txt
